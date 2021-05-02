20083801f136218dc0b033479520a0284604f6ab
Shane Slone <slone.shane@gmail.com>
2021-05-01 23:45:54 -0400

    cleared main class

--- StacksAndQueues/src/com/lambdaschool/stacksandqueues/Main.java
+++ StacksAndQueues/src/com/lambdaschool/stacksandqueues/Main.java
@@ -2,16 +2,5 @@ package com.lambdaschool.stacksandqueues;
 
 public class Main {
     public static void main(String[] args) {
-        AnimalQueue animals = new AnimalQueue();
-        animals.enqueue(new Dog("Fido"));
-        animals.enqueue(new Dog("Nasus"));
-        animals.enqueue(new Cat("Nidalee"));
-        animals.enqueue(new Dog("Warwick"));
-        animals.enqueue(new Cat("Rengar"));
-
-        System.out.println(animals.dequeueAny().name);
-        System.out.println(animals.dequeueCat().name);
-        System.out.println(animals.dequeueCat().name);
-        System.out.println(animals.dequeueDog().name);
     }
 }
